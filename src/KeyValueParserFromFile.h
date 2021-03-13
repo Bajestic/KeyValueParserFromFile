a055236543970fbe144102a99eab6f789cb685f8
Bajestic <lukasz.przybyla@hotmail.com>
2021-03-13 15:56:46 +0100

    avaible std::string and int pair types parser

--- src/KeyValueParserFromFile.h
+++ src/KeyValueParserFromFile.h
@@ -1,6 +1,18 @@
 #ifndef KEYVALUEPARSERFROMFILE_H_
 #define KEYVALUEPARSERFROMFILE_H_
 
+/*
+/ Key Value Parser From File is one header file which add parser
+/ functions for you project to extract data from file to containers.
+/ To use call ParseFileToVector() function and make template specialization
+/ depending on the types to be guessed from file.
+/ (ParseFileToOtherContainers() functions in progress)
+/ It is also possible to choose two separators - 'assigment separator',
+/ connect key and value in pair and 'separator' to split next key-value pair.
+/ Default separators are respectively '=' for assigment separator
+/ and next line tag '\n' for separator
+*/
+
 #include <fstream>
 #include <vector>
 #include <map>
@@ -8,59 +20,67 @@
 #include <cstring>
 #include <type_traits>
 
-// TODO: Make struct of bools to easly manage types
 // TODO: More static assertions for type control to choose overload function for strict types
-// TODO: Extract data to containers - map and vector of pairs
+// TODO: Extract data to containers - map and vector of pairs for more types
 // TODO: Static_assert vs Enable_if, which better for multiply clarifications types ?
-// TODO: Tests and exceptions...
+// TODO: Exceptions in case of bad format source file (repeated separators)
+// TODO: Tests...
 
 template< typename key_type, typename values_type >
-std::vector<std::pair< key_type, values_type >> ParseFileToVector(std::string filename, const char assigment_separator = '=', const char separator = '\n')
+std::vector<std::pair< key_type, values_type >> ParseFileToVector(std::string filename,
+            const char assigment_separator = '=', const char separator = '\n')
 {
-    static_assert( std::is_same< key_type, std::string >::value &&
-                   std::is_same< values_type,   int >::value,
-                   "Key type is not a string type or value type is not a int type\n" );
+    // for now only support <std::string, int>
+    const bool key_type_is_string     = std::is_same< key_type, std::string >::value;
+    const bool value_type_is_int      = std::is_same< values_type, int >::value;
+    static_assert( key_type_is_string && value_type_is_int, "Unsupported types" );
+}
 
-//    bool key_type_is_string = std::is_same< key_type, std::string >::value;
+template<>
+std::vector<std::pair< std::string, int >> ParseFileToVector(std::string filename,
+            const char assigment_separator, const char separator)
+{
+    std::ifstream parse_file(filename, std::ios::in);
+    std::vector<std::pair<std::string,int>> vec;
+    std::pair<std::string,int> pair_temp{};
+    std::string key_temp{};
+    int values_temp{};
+    char sepeparators[2] = { assigment_separator, separator };
+    bool key_value_switcher = true;
 
-    if( std::is_same< key_type, std::string >::value && std::is_same< values_type, int >::value )
+    if(parse_file.is_open())
     {
-        std::ifstream parse_file(filename, std::ios::in);
-        std::vector<std::pair<key_type,values_type>> vec;
-        const char sep = separator;
-        key_type key{};
-        values_type values{};
-        if(parse_file.is_open())
+        std::string temp;
+        temp.assign( std::istreambuf_iterator<char>(parse_file), std::istreambuf_iterator<char>() );
+        char * fileTemp = new char[temp.length() + 1];
+        strcpy( fileTemp,temp.c_str() );
+        char * token = strtok(fileTemp, &assigment_separator);
+        while( token != NULL )
         {
-            std::string temp;
-            temp.assign( std::istreambuf_iterator<char>(parse_file), std::istreambuf_iterator<char>() );
-            char * fileTemp = new char[temp.length() + 1];
-            strcpy( fileTemp,temp.c_str() );
-            char * token = strtok(fileTemp, &assigment_separator);
-            std::cout << token;
-            while( token != NULL )
+            if( key_value_switcher )
             {
-                // control message, replace std::cout with push_back()
-                std::cout << token << '\n';
-                token = std::strtok( NULL, "=\n" );
+                pair_temp.first = token;
+                key_value_switcher = false;
             }
-            delete [] fileTemp;
-        }
-        else
-        {
-            std::cerr << "File not found\n";
-            return vec;
-        }
+            else
+            {
+                pair_temp.second = atoi(token);
+                vec.push_back(pair_temp);
+                key_value_switcher = true;
+            }
 
-        parse_file.close();
-        return vec;
+            token = std::strtok( NULL, sepeparators );
+        }
+        delete [] fileTemp;
     }
     else
     {
-        std::cerr << "Fail";
-        std::vector<std::pair<key_type,values_type>> vec;
+        std::cerr << "File not found\n";
         return vec;
     }
+
+    parse_file.close();
+    return vec;
 }
 
 #endif // KEYVALUEPARSERFROMFILE_H_
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
+#include <algorithm>
 #include "KeyValueParserFromFile.h"
+// Add header file
 
 int main()
 {
-    ParseFileToVector<std::string, int>("demo.txt", '=');
+    // make specialization and choose separators
+    auto out = ParseFileToVector<std::string, int>("demo.txt",'=','\n');
+
+    // Use
+    std::cout << "Original data:\n";
+    for(auto element : out)
+        std::cout << element.first << " = " << element.second << '\n';
+
+    std::cout << "\nNumber is multiply by 3:\n";
+    for(auto element : out)
+        std::cout << element.first << " = " << element.second * 3 << '\n';
+
+    std::cout << "\nErase 'e' letter from text part and add 5\n";
+    for(auto element : out)
+    {
+        element.first.erase(std::remove(element.first.begin(), element.first.end(), 'E'),
+                                         element.first.end());
+        std::cout << element.first << " = " << element.second + 5 << '\n';
+    }
+
 
     return 0;
 }
